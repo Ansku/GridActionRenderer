5f89824bfd8751783bf4863e2d7c8fb1a5c26408
Thorben von Hacht <thorben.vonhacht@bonprix.net>
2017-02-08 11:13:37 +0100

    fw-128: gridaction can now take resources instead of fontawesome

--- gridactionrenderer-addon/src/main/java/org/vaadin/anna/gridactionrenderer/GridAction.java
+++ gridactionrenderer-addon/src/main/java/org/vaadin/anna/gridactionrenderer/GridAction.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.vaadin.event.Action;
-import com.vaadin.server.FontAwesome;
+import com.vaadin.server.Resource;
 import com.vaadin.server.ThemeResource;
 
 /**
@@ -31,122 +31,122 @@ import com.vaadin.server.ThemeResource;
  */
 public class GridAction extends Action {
 
-    private String description;
-    private boolean download = false;
-    private List<String> styleNames = new ArrayList<String>();
+	private String description;
+	private boolean download = false;
+	private List<String> styleNames = new ArrayList<String>();
 
-    /**
-     * Constructor.
-     *
-     * @param iconPath
-     *            path for a ThemeResource icon, required
-     * @param description
-     *            tooltip text, can be null
-     */
-    public GridAction(String iconPath, String description) {
-        super(null);
-        assert iconPath != null;
+	/**
+	 * Constructor.
+	 *
+	 * @param iconPath
+	 *            path for a ThemeResource icon, required
+	 * @param description
+	 *            tooltip text, can be null
+	 */
+	public GridAction(String iconPath, String description) {
+		super(null);
+		assert iconPath != null;
 
-        setIcon(new ThemeResource(iconPath));
-        this.description = description;
-    }
+		setIcon(new ThemeResource(iconPath));
+		this.description = description;
+	}
 
-    /**
-     * Constructor.
-     *
-     * @param icon
-     *            FontAwesome icon, required
-     * @param description
-     *            tooltip text, can be null
-     */
-    public GridAction(FontAwesome icon, String description) {
-        super(null);
-        assert icon != null;
+	/**
+	 * Constructor.
+	 *
+	 * @param icon
+	 *            Resource icon, required
+	 * @param description
+	 *            tooltip text, can be null
+	 */
+	public GridAction(Resource icon, String description) {
+		super(null);
+		assert icon != null;
 
-        setIcon(icon);
-        this.description = description;
-    }
+		setIcon(icon);
+		this.description = description;
+	}
 
-    /**
-     * Constructor. If you want to give the action a tooltip, use
-     * {@link #GridAction(String, String)} instead.
-     *
-     * @param iconPath
-     *            path for a ThemeResource icon, required
-     */
-    public GridAction(String iconPath) {
-        this(iconPath, null);
-    }
+	/**
+	 * Constructor. If you want to give the action a tooltip, use
+	 * {@link #GridAction(String, String)} instead.
+	 *
+	 * @param iconPath
+	 *            path for a ThemeResource icon, required
+	 */
+	public GridAction(String iconPath) {
+		this(iconPath, null);
+	}
 
-    /**
-     * Constructor. If you want to give the action a tooltip, use
-     * {@link #GridAction(FontAwesome, String)} instead.
-     *
-     * @param icon
-     *            FontAwesome icon, required
-     */
-    public GridAction(FontAwesome icon) {
-        this(icon, null);
-    }
+	/**
+	 * Constructor. If you want to give the action a tooltip, use
+	 * {@link #GridAction(Resource, String)} instead.
+	 *
+	 * @param icon
+	 *            Resource icon, required
+	 */
+	public GridAction(Resource icon) {
+		this(icon, null);
+	}
 
-    /**
-     * Returns the description that is displayed as a tooltip for the action
-     * icon.
-     *
-     * @return description
-     */
-    public String getDescription() {
-        return description;
-    }
+	/**
+	 * Returns the description that is displayed as a tooltip for the action
+	 * icon.
+	 *
+	 * @return description
+	 */
+	public String getDescription() {
+		return this.description;
+	}
 
-    /**
-     * Sets the description that is displayed as a tooltip for the action icon.
-     *
-     * @param description
-     *            tooltip text, can be null
-     */
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	/**
+	 * Sets the description that is displayed as a tooltip for the action icon.
+	 *
+	 * @param description
+	 *            tooltip text, can be null
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    /**
-     * Marks the action as a download action.
-     * <p>
-     * NOTE: this method shouldn't be called except from
-     * {@link GridDownloadAction}.
-     */
-    protected void markDownloadAction() {
-        download = true;
-    }
+	/**
+	 * Marks the action as a download action.
+	 * <p>
+	 * NOTE: this method shouldn't be called except from
+	 * {@link GridDownloadAction}.
+	 */
+	protected void markDownloadAction() {
+		this.download = true;
+	}
 
-    /**
-     * Returns {@code true} if the action is a download action, {@code false}
-     * otherwise.
-     *
-     * @return {@code true} if download action, {@code false} otherwise.
-     */
-    public boolean isDownloadAction() {
-        return download;
-    }
+	/**
+	 * Returns {@code true} if the action is a download action, {@code false}
+	 * otherwise.
+	 *
+	 * @return {@code true} if download action, {@code false} otherwise.
+	 */
+	public boolean isDownloadAction() {
+		return this.download;
+	}
 
-    /**
-     * Adds a custom style name for the action icon.
-     * <p>
-     * NOTE: the styles can't be changed on the fly
-     *
-     * @param styleName
-     *            custom style name
-     */
-    public void addStyleName(String styleName) {
-        styleNames.add(styleName);
-    }
+	/**
+	 * Adds a custom style name for the action icon.
+	 * <p>
+	 * NOTE: the styles can't be changed on the fly
+	 *
+	 * @param styleName
+	 *            custom style name
+	 */
+	public void addStyleName(String styleName) {
+		this.styleNames.add(styleName);
+	}
 
-    /**
-     * Returns the custom style names that have been added for the action icon.
-     *
-     * @return list of custom style names
-     */
-    public List<String> getStyleNames() {
-        return new ArrayList<String>(styleNames);
-    }
+	/**
+	 * Returns the custom style names that have been added for the action icon.
+	 *
+	 * @return list of custom style names
+	 */
+	public List<String> getStyleNames() {
+		return new ArrayList<String>(this.styleNames);
+	}
 }
